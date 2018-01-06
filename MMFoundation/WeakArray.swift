0e295ab311ece95f70a88cb06d300d298b515c20
Michał Moskała <moskala.michal@gmail.com>
2018-01-06 12:32:42 +0100

    WeakArray added to project. This class holds array of WeakBox<T> objects.

---
+++ MMFoundation/WeakArray.swift
@@ -0,0 +1,22 @@
+//
+//  WeakArray.swift
+//  MMFoundation
+//
+//  Created by Michal Moskala on 06.01.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+class WeakArray<T:AnyObject> {
+    
+    private var items: [WeakBox<T>]
+    
+    public init(items: [T] = []) {
+        self.items = items.map { WeakBox(value: $0) }
+    }
+    
+    public func add(item: T) {
+        items.append(WeakBox(value: item))
+    }
+}
