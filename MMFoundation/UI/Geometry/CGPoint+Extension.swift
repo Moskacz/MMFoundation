86f97984107103940b23452cc5f911719a95ab2d
Michał Moskała <moskala.michal@gmail.com>
2018-03-24 15:53:28 +0100

    Added cubic points to CGPoint. Added convenience initializer to Gradient struct

---
+++ MMFoundation/UI/Geometry/CGPoint+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  CGPoint+Extension.swift
+//  MMFoundation
+//
+//  Created by Michal Moskala on 24.03.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+extension CGPoint {
+    
+    public class Cubic {
+        public var topLeft: CGPoint { return CGPoint(x: 0, y: 0) }
+        public var topRight: CGPoint { return CGPoint(x: 1, y: 0) }
+        public var bottomLeft: CGPoint { return CGPoint(x: 0, y: 1) }
+        public var bottomRight: CGPoint { return CGPoint(x: 1, y: 1) }
+    }
+    
+    public static let cubic = Cubic()
+}
--- MMFoundation/UI/Gradient/GradientRepresenting.swift
+++ MMFoundation/UI/Gradient/GradientRepresenting.swift
@@ -18,4 +18,40 @@ public struct Gradient: GradientRepresenting {
     public var colors: [UIColor]
     public var startPoint: CGPoint
     public var endPoint: CGPoint
+    
+    public init(colors: [UIColor], startPoint: CGPoint, endPoint: CGPoint) {
+        self.colors = colors
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+    }
+    
+    public init(colors: [UIColor], direction: GradientDirection) {
+        let points = direction.points
+        self.init(colors: colors, startPoint: points.startPoint, endPoint: points.endPoint)
+    }
+}
+
+public struct GradientPoints {
+    public var startPoint: CGPoint
+    public var endPoint: CGPoint
+}
+
+public enum GradientDirection {
+    case vertical
+    case horizontal
+    case skewRight
+    case skewLeft
+    
+    public var points: GradientPoints {
+        switch self {
+        case .vertical:
+            return GradientPoints(startPoint: CGPoint.cubic.topLeft, endPoint: CGPoint.cubic.bottomLeft)
+        case .horizontal:
+            return GradientPoints(startPoint: CGPoint.cubic.topLeft, endPoint: CGPoint.cubic.topRight)
+        case .skewRight:
+            return GradientPoints(startPoint: CGPoint.cubic.topLeft, endPoint: CGPoint.cubic.bottomRight)
+        case .skewLeft:
+            return GradientPoints(startPoint: CGPoint.cubic.topRight, endPoint: CGPoint.cubic.bottomLeft)
+        }
+    }
 }
