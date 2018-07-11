4106aa71c41c3ead0a71f871601107f5d60b7a11
Michał Moskała <moskala.michal@gmail.com>
2018-07-11 20:07:41 +0200

    Added ResultsController abstract class

---
+++ MMFoundation/ResultsController/ResultsController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsController.swift
+//  MMFoundation
+//
+//  Created by Michal Moskala on 11.07.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+public class ResultsController<T> {
+    func loadData() throws { fatalError("abstract method")}
+    func object(at indexPath: IndexPath) -> T { fatalError("abstract method") }
+    var sectionsCount: Int { fatalError("abstract method") }
+    func objects(in section: Int) -> [T] { fatalError("abstract method")}
+}
