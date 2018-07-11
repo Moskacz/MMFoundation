8522d1c7485a80a30fe74eec0416a23f82540f16
Michał Moskała <moskala.michal@gmail.com>
2018-07-11 20:25:04 +0200

    Introduced ResultsControllerDelegate protocol

--- MMFoundation/ResultsController/ResultsController.swift
+++ MMFoundation/ResultsController/ResultsController.swift
@@ -9,8 +9,23 @@
 import Foundation
 
 public class ResultsController<T> {
+    weak var delegate: ResultsControllerDelegate?
     func loadData() throws { fatalError("abstract method")}
     func object(at indexPath: IndexPath) -> T { fatalError("abstract method") }
     var sectionsCount: Int { fatalError("abstract method") }
     func objects(in section: Int) -> [T]? { fatalError("abstract method")}
+}
+
+public protocol ResultsControllerDelegate: class {
+    func resultsControllerWillChangeContent()
+    func resultsControllerDidChangeContent()
+    func resultsControllerDid(change: ResultChangeType, section: Int)
+    func resultsControllerDid(change: ResultChangeType, itemAtPath oldPath: IndexPath?, to newPath: IndexPath?)
+}
+
+public enum ResultChangeType {
+    case insert
+    case delete
+    case update
+    case move
 }
