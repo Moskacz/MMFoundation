8d304faa82154933c2daea146f1ef5c68b8c6edb
Michał Moskała <moskala.michal@gmail.com>
2018-11-18 18:53:27 +0100

    Add ListAdapter class

---
+++ Sources/List/ListAdapter.swift
@@ -0,0 +1,16 @@
+//
+//  ListAdapter.swift
+//  MMFoundation
+//
+//  Created by Michal Moskala on 18/11/2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+open class ListAdapter<Item> {
+    open var numberOfSections: Int { fatalError() }
+    open func numberOfRows(in section: Int) -> Int { fatalError() }
+    open func item(at indexPath: IndexPath) -> Item { fatalError() }
+    open func title(for section: Int) -> String { fatalError() }
+}
