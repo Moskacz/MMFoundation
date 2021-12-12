d16e6d1612bdf9806540e817ac595562970bf203
Michał Moskała <moskala.michal@gmail.com>
2021-12-12 22:30:50 +0100

    Register for notifications

--- Sources/MMFoundation/MMKeyboardObserver.swift
+++ Sources/MMFoundation/MMKeyboardObserver.swift
@@ -7,16 +7,46 @@
 
 import UIKit
 
+protocol MMKeyboardObserverDelegate: AnyObject {
+    func keyboard(observer: MMKeyboardObserver, didDetectKeyboardFrameChange frame: CGRect)
+}
+
 final class MMKeyboardObserver {
+    weak var delegate: MMKeyboardObserverDelegate?
     
+    private let notificationCenter: NotificationCenter
     private weak var scrollView: UIScrollView?
+    private var notificationObservers = [NSObjectProtocol]()
     
     init(notificationCenter: NotificationCenter, scrollView: UIScrollView? = nil) {
+        self.notificationCenter = notificationCenter
         self.scrollView = scrollView
-        registerForNotifications(using: notificationCenter)
+        self.notificationObservers = registerForNotifications(using: notificationCenter)
     }
     
-    private func registerForNotifications(using notificationCenter: NotificationCenter) {
+    deinit {
+        notificationObservers.forEach {
+            notificationCenter.removeObserver($0)
+        }
+    }
+    
+    private func registerForNotifications(using notificationCenter: NotificationCenter) -> [NSObjectProtocol] {
+        let showObserver = notificationCenter.addObserver(forName: UIApplication.keyboardWillShowNotification, object: self, queue: .main) { [weak self] note in
+            self?.handle(notification: note)
+        }
+        
+        let hideObserver = notificationCenter.addObserver(forName: UIApplication.keyboardWillHideNotification, object: self, queue: .main) { [weak self] note in
+            self?.handle(notification: note)
+        }
+        
+        let changeObserver = notificationCenter.addObserver(forName: UIApplication.keyboardWillChangeFrameNotification, object: self, queue: .main) { [weak self] note in
+            self?.handle(notification: note)
+        }
+        
+        return [showObserver, hideObserver, changeObserver]
+    }
+    
+    private func handle(notification: Notification) {
         
     }
 }
